b0ff8cda102ff968054bd1917ef93b728f9e6ad9
olliert1 <60087390+olliert1@users.noreply.github.com>
2020-01-20 01:47:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Thomas Ollier</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+$name = "Thomas Ollier";
+$email = "olliert1@nku.edu";
+$major = "CIT";
+$year = "Junior?";
+?>
+
+<h1> About Me </h1>
+<p>
+Name: <?=$name?></br>
+Email: <?=$email?></br>
+Major: <?=$major?></br>
+Year: <?=$year?>
+</p>
+</p> Hello, I'm Thomas. I've been going to NKU for a while. My hobbies include video games, reading, rock climbing, and music. I work at a car dealership in accounting. I read tons of comic books, mostly foreign. I have spent two summers on tour with a drum corps, and drove 8 hours every weekend to do drumline in a gym for 5 months.</p>
+
+<img src="apicturesofme.jpg" height="300px" alt="Me">
+
+<p>I enrolled in this class because it was one of my requisites for my major and it seemed like a good choice given how well I enjoyed intro to Web Dev. Web Dev is also pretty fun because you can watch your work take shape as you slowly build onto it. By the end of this class I expect to feel better about designing web pages and not just mindlessly copy pasting generic bulk code together.</p>
+
+
+</body>
+</html>
